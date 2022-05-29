4c29f9473caaeded58f55d78b6c8cb37520ff6a7
Jeanne Franco <44760301+francojra@users.noreply.github.com>
2022-05-29 20:33:44 -0300

    Add files via upload

---
+++ static_bubble_map.R
@@ -0,0 +1,14 @@
+
+# Static bubble map ------------------------------------------------------------------------------------------------------------------------
+# Autoria do script: Jeanne Franco ---------------------------------------------------------------------------------------------------------
+# Data: 29/05/22 ---------------------------------------------------------------------------------------------------------------------------
+# Referência: The R Graph Galery -----------------------------------------------------------------------------------------------------------
+
+# Conceito ---------------------------------------------------------------------------------------------------------------------------------
+
+### ggplot2 é provavelmente a melhor opção para construir um estastíco bubble map.
+### Ele oferece toda flexibilidade da gramática de gráficos, e permite reusar todo
+### conhecimento que você aprendeu construindo outros tipos de gráficos. 
+
+### Ele usa uma lista de coordenadas de GPS e adiciona no topo do mapa com tamanho
+### e cor mapeados de acordo com os dados.
